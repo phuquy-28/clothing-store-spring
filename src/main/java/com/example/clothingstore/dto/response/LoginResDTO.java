1f1946f1081e03013b97db5837937c0168dfd1b0
phuquy-28 <phuquypro.2003@gmail.com>
2025-05-25 14:15:55 +0700

    fix(auth): add @JsonIgnore annotation to refreshToken in LoginResDTO to prevent serialization; ensure sensitive data is not exposed in API responses

--- src/main/java/com/example/clothingstore/dto/response/LoginResDTO.java
+++ src/main/java/com/example/clothingstore/dto/response/LoginResDTO.java
@@ -1,5 +1,6 @@
 package com.example.clothingstore.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +15,7 @@ public class LoginResDTO {
   private String accessToken;
 
   @JsonProperty("refresh_token")
+  @JsonIgnore
   private String refreshToken;
 
   private ResUser user;
