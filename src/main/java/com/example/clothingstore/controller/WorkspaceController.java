9d75f14c0b5629994d82469fdf0a213c4136a6ca
phuquy-28 <phuquypro.2003@gmail.com>
2025-06-01 13:12:53 +0700

    feat(auth): enhance login functionality by adding refresh token handling; implement cookie creation for secure token storage in WorkspaceController and log cookie details for debugging

--- src/main/java/com/example/clothingstore/controller/WorkspaceController.java
+++ src/main/java/com/example/clothingstore/controller/WorkspaceController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import com.example.clothingstore.constant.AppConstant;
 import com.example.clothingstore.constant.UrlConfig;
 import com.example.clothingstore.dto.request.LoginReqDTO;
 import com.example.clothingstore.dto.response.DashboardResDTO;
@@ -23,20 +25,38 @@ import com.example.clothingstore.enumeration.ImportMode;
 import com.example.clothingstore.enumeration.TemplateType;
 import com.example.clothingstore.service.ImportService;
 import com.example.clothingstore.service.WorkspaceService;
+import com.example.clothingstore.util.SecurityUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("${api.version}")
 @RequiredArgsConstructor
 public class WorkspaceController {
 
+  private final Logger log = LoggerFactory.getLogger(WorkspaceController.class);
   private final WorkspaceService workspaceService;
   private final ImportService importService;
+  private final SecurityUtil securityUtil;
 
   @PostMapping(UrlConfig.WORKSPACE + UrlConfig.LOGIN)
   public ResponseEntity<LoginResDTO> login(@RequestBody @Valid LoginReqDTO loginReqDTO) {
-    return ResponseEntity.ok(workspaceService.login(loginReqDTO));
+    LoginResDTO loginResDTO = workspaceService.login(loginReqDTO);
+
+    // Create refresh token
+    String refreshToken = securityUtil.createRefreshToken(loginReqDTO.getEmail(), loginResDTO);
+
+    // Create cookie
+    ResponseCookie springCookie = ResponseCookie
+        .from(AppConstant.REFRESH_TOKEN_COOKIE_NAME, refreshToken).httpOnly(true).secure(true)
+        .path("/").maxAge(AppConstant.REFRESH_TOKEN_COOKIE_EXPIRE).sameSite("Lax").build();
+
+    // Return response
+    log.debug("Set refresh token cookie: {}", springCookie.toString());
+    return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, springCookie.toString())
+        .body(loginResDTO);
   }
 
   @GetMapping(UrlConfig.WORKSPACE + UrlConfig.DASHBOARD)
